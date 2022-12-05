4a419ff9503ef8d1a1a9ba7e6a2fa479571a2181
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2022-12-05 12:35:12 +0500

    navbar end

---
+++ src/componenets/navbar/navbar.jsx
@@ -0,0 +1,36 @@
+import "./navbar.css";
+
+// images
+import Logo from "../../assets/img/Logo.png";
+import Search from "../../assets/img/search.png";
+import Storage from "../../assets/img/storage.png";
+
+
+
+const Navbar = () =>{
+    return(
+        <div className="navbar">
+            <div className="container">
+                <div className="nav-box">
+                    <a className="nav-logo" href="/"><img src={Logo} alt="Green"/></a>
+                    
+                    <ul className="nav-list">
+                        <li className="nav-item"><a className="nav-item-link" href="#">Home</a></li>
+                        <li className="nav-item"><a className="nav-item-link" href="#">Shop</a></li>
+                        <li className="nav-item"><a className="nav-item-link" href="#">Plant Care</a></li>
+                        <li className="nav-item"><a className="nav-item-link" href="#">Blogs</a></li>
+                    </ul>
+
+                    <div className="nav-btn-box">
+                        <button className="nav-search"><img src={Search} alt=""/></button>
+                        <button className="nav-search"><img src={Storage} alt="" /></button>
+                        <button className="nav-login"><img src={Search} alt="" />  <p>  Log in</p></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Navbar;
---
+++ src/pages/homePage/homepage.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../../componenets/navbar/navbar";
+import "./homepage.css";
+
+
+
+const HomePage = () =>{
+    return(
+        <>
+        <Navbar/>
+        </>
+    )
+}
+
+
+export default HomePage;
