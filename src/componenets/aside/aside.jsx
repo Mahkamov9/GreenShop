09af41e853615020c02b0fbb3657008c46a549de
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2022-12-05 16:39:40 +0500

    sidebar

--- src/componenets/aside/aside.jsx
+++ src/componenets/aside/aside.jsx
@@ -1,5 +1,7 @@
 import './aside.css';
 
+import Supersale from "../../assets/img/supersale.png"
+
 // Pagination
 
 import * as React from 'react';
@@ -8,6 +10,7 @@ import Stack from '@mui/material/Stack';
 
 
 import Cardimg from "../../assets/img/card-img.png";
+import RangeSlider from '../slider/slider';
 
 const Aside = () =>{
     return(
@@ -16,19 +19,39 @@ const Aside = () =>{
                 <div className="aside-box">
                     
                     <div className='aside-sidebar'>
-                        <h4>Categories</h4>
+                        <h4 className='aside-sidebar-subtitlte' >Categories</h4>
                         <ul>
-                            <li>House Plants</li>
-                            <li>House Plants</li>
-                            <li>House Plants</li>
-                            <li>House Plants</li>
-                            <li>House Plants</li>
-                            <li>House Plants</li>
-                            <li>House Plants</li>
-                            <li>House Plants</li>
-                            <li>House Plants</li>
-
+                            <li className='aside-sidebar-item' ><p>House Plants</p>  <p>(33)</p> </li>
+                            <li className='aside-sidebar-item' ><p>Potter Plants</p>  <p>(12)</p> </li>
+                            <li className='aside-sidebar-item' ><p>Seeds</p>  <p>(65)</p> </li>
+                            <li className='aside-sidebar-item' ><p>Small Plants</p>  <p>(39)</p> </li>
+                            <li className='aside-sidebar-item' ><p>Big Plants</p>  <p>(23)</p> </li>
+                            <li className='aside-sidebar-item' ><p>Succelents</p>  <p>(17)</p> </li>
+                            <li className='aside-sidebar-item' ><p>Trerraiums</p>  <p>(19)</p> </li>
+                            <li className='aside-sidebar-item' ><p>Gardening</p>  <p>(13)</p> </li>
+                            <li className='aside-sidebar-item' ><p>Accessories</p>  <p>(18)</p> </li>
                         </ul>
+
+                        <h4 className='aside-sidebar-subtitlte' >Price Range</h4>
+                        
+                        <div className='sidebar-slider' >
+                            <RangeSlider/>
+                            <p className='card-price'> <span>Price:</span> $39 - $1230</p>
+                        </div>
+
+                        <button className='sidebar-filter'>Filter</button>
+
+                        <h4 className='aside-sidebar-subtitlte' >Size</h4>
+
+                        <ul >
+                            <li className='aside-sidebar-item' ><p>Small</p>  <p>(119)</p> </li>
+                            <li className='aside-sidebar-item' ><p>Medium</p>  <p>(86)</p> </li>
+                            <li className='aside-sidebar-item' ><p>Large</p>  <p>(78)</p> </li>
+                        </ul>
+
+                        <img className='sidebar-img' src={Supersale} alt="" />
+
+
                     </div>
                     <div className='aside-product'>
                         <div className='aside-product-nav'>
@@ -101,14 +124,46 @@ const Aside = () =>{
                                     <p className='card-price'>$129.00</p>
                                 </div>
                             </li>
+
+                            <li>
+                                <div>
+                                    <div className='aside-card'>
+                                        <img src={Cardimg} alt="" />
+                                    </div>
+                                    <p className='card-text'>Beach Spider Lidy</p>
+                                    <p className='card-price'>$129.00</p>
+                                </div>
+                            </li>
+
+                            <li>
+                                <div>
+                                    <div className='aside-card'>
+                                        <img src={Cardimg} alt="" />
+                                    </div>
+                                    <p className='card-text'>Beach Spider Lidy</p>
+                                    <p className='card-price'>$129.00</p>
+                                </div>
+                            </li>
+
+                            <li>
+                                <div>
+                                    <div className='aside-card'>
+                                        <img src={Cardimg} alt="" />
+                                    </div>
+                                    <p className='card-text'>Beach Spider Lidy</p>
+                                    <p className='card-price'>$129.00</p>
+                                </div>
+                            </li>
+ 
                         </ul>
 
                         <div className='aside-pagination' >
                         <Stack 
                             spacing={4}>
-                            <Pagination count={4} variant="outlined" shape="rounded" />
+                            <Pagination count={4}  shape="rounded" color="success" hidePrevButton  />
                         </Stack>
                         </div>
+
 
                     </div>
                 </div>
