57a28ad5010796bf3b68a836d9b272eef5ebf77e
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2022-12-05 21:02:04 +0500

    article end

--- src/componenets/article/article.jsx
+++ src/componenets/article/article.jsx
@@ -1,7 +1,13 @@
 import './article.css';
 
-import image from "../../assets/img/image.png"
-import blogimg from "../../assets/img/blogimg.png"
+import image from "../../assets/img/image.png";
+import blogimg from "../../assets/img/blogimg.png";
+import cactus from "../../assets/img/cactus.png";
+import bottomLogo from '../../assets/img/Logo.png';
+
+import location from '../../assets/img/Location.png';
+import mail from '../../assets/img/Message.png';
+import phone from '../../assets/img/Calling.png';
 
 
 const Article = () =>{
@@ -68,6 +74,47 @@ const Article = () =>{
                             <a className='blog-link' href="#">Read More</a>
                         </li>
                     </ul>
+                </div>
+
+                <div className="section">
+                    <div className="section-box">
+                        <div className='section-box-card'>
+                            <img className='section-box-img' src={cactus} alt="" />
+                            <p className='section-box-subtitle' >Garden Care</p>
+                            <p className='section-box-title'>We are an online plant shop offering a wide range of cheap and trendy plants.</p>
+                        </div>
+                        <div className='section-box-card'>
+                            <img className='section-box-img' src={cactus} alt="" />
+                            <p className='section-box-subtitle' >Garden Care</p>
+                            <p className='section-box-title'>We are an online plant shop offering a wide range of cheap and trendy plants.</p>
+                        </div>
+                        <div className='section-box-card'>
+                            <img className='section-box-img' src={cactus} alt="" />
+                            <p className='section-box-subtitle' >Garden Care</p>
+                            <p className='section-box-title'>We are an online plant shop offering a wide range of cheap and trendy plants.</p>
+                        </div>                        
+                        <div className='section-box-input'>
+                            <p className='section-box-input-subtite'>Would you like to join newsletters?</p>
+                            <div>
+                                <input className='section-input'  type="text" placeholder='enter your email address...' />
+                                <button className='section-btn' >Join</button>
+                            </div>
+                            <p className='section-box-input-tite'>We usually post offers and challenges in newsletter. We’re your online houseplant destination. We offer a wide range of houseplants and accessories shipped directly from our (green)house to yours! </p>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="section">
+                    <div className="section-bottom-box">
+                        <ul className='section-bottom-box-list'>
+                            <li className='section-bottom-box-item'><img src={bottomLogo} alt="" /></li>
+                            <li className='section-bottom-box-item'><img src={location} alt="" /> <p className='section-bottom-box-item-link'>70 West Buckingham <br /> Ave.Farmingdale, NY 11735</p></li>
+                            <li className='section-bottom-box-item'><img src={mail} alt="" /> <a className='section-bottom-box-item-link' href="#">contact@greenshop.com</a></li>
+                            <li className='section-bottom-box-item'><img src={phone} alt="" /> <a className='section-bottom-box-item-link' href="#">+88 01911 717 490</a></li>
+                            
+                        </ul>
+
+                    </div>
                 </div>
 
 
